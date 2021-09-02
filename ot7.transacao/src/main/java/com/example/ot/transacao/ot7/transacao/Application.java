25990a9e67ed94568b2d0a126da36eade70c8aa0
GabrielFerreiraZup <gabriel.mendes@zup.com.br>
2021-09-02 14:39:00 -0300

    setup projeto

---
+++ ot7.transacao/src/main/java/com/example/ot/transacao/ot7/transacao/Application.java
@@ -0,0 +1,15 @@
+package com.example.ot.transacao.ot7.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+		System.out.println("It works");
+	}
+
+}
